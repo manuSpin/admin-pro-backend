9b9f8810529ad8165d992e83838683a42744b93a
Manuel Fernández <manuel.fcarmona.91@gmail.com>
2024-06-13 12:33:24 +0200

    Signup con Google

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/usuario.model');
 const bcrypt = require('bcryptjs');
 const { generateTokenJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (request, res = response) => {
@@ -36,5 +37,46 @@ const login = async (request, res = response) => {
     }
 }
 
-module.exports = { login }
+const loginWithGoogle = async (request, res = response) => {
+    try {
+        const { email, name, picture } = await googleVerify(request.body.token);
+
+        const usuarioDB = await Usuario.findOne({ email: email });
+        let usuario;
+
+        if (!usuarioDB) {
+            usuario = new Usuario({ 
+                nombre: name,
+                email: email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        await usuario.save();
+
+        const token = await generateTokenJWT(usuario.id);
+
+        res.json({
+            ok: true,
+            email: email,
+            name: name,
+            picture: picture,
+            token: token
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            error: 'Error: ' + error
+        });
+
+    }
+}
+
+module.exports = { login, loginWithGoogle }
 
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
 // Ruta:/api/login
 const { Router } = require('express');
-const { login } = require('../controllers/auth.controller');
+const { login, loginWithGoogle } = require('../controllers/auth.controller');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
@@ -13,5 +13,11 @@ router.post('/', [
     validarCampos
 
 ], login)
+
+router.post('/google', [
+    check('token', 'El token de Google es obligatorio.').not().isEmpty(),
+    validarCampos
+
+], loginWithGoogle)
 
 module.exports = router;
